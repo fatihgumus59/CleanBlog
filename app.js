62cb47714ec721f243fd86d2bb013ba68ff47800
fatihgumus59 <fatihgumus72@gmail.com>
2022-02-01 15:56:04 +0300

    template engine

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
+  res.render('index', { title: 'Clean Blog' });
+});
 
+app.get('/about', (req, res) => {
+  res.render('about',{title: "About"});
+});
 
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.get('/post', (req, res) => {
+  res.render('post',{title:"Post"});
+});
 
-  res.send(blog);
+app.get('/add_post', (req, res) => {
+  res.render('add_post',{title:"Add Post"});
 });
 
 const port = 3000;
